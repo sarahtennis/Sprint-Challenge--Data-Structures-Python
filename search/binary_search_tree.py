42da4e9dd1c0765200fced3ab4589c9ba63aaf09
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-03-01 11:45:02 -0500

    completes breadth first for BST

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -4,11 +4,36 @@ class BinarySearchTree:
     self.left = None
     self.right = None
 
+  # start at root node, explore as far on each branch before back tracking
   def depth_first_for_each(self, cb):
     pass    
 
+  # call anon function on each in breadth first order
+  # traverse nodes in order
+  # HINT: probably want to utilize a Queue data structure (FIFO)
+  # put first node in queue --> pop --> if left child push, if right child push --> continue
   def breadth_first_for_each(self, cb):
-    pass
+    queue = [self]
+
+    while True:
+      # if node in queue
+      if len(queue):
+        # take front node
+        current = queue.pop(0)
+
+        # check children
+        if current.left:
+          queue.append(current.left)
+        if current.right:
+          queue.append(current.right)
+
+        # run anon function
+        cb(current.value)
+      else:
+        break
+
+
+
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
