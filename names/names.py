442b87606db582f808e6b82a30d5fca9895677e0
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-03-01 13:28:39 -0500

    refactors names.py to run at O(n) time

--- names/names.py
+++ names/names.py
@@ -2,6 +2,11 @@ import time
 
 start_time = time.time()
 
+# text files are unordered, new name on each line
+# Hint: You might try importing a data structure you built during the week
+# duplicates: 64
+# non duplicates: 19876
+
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -10,13 +15,31 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# O(n)
+all_names = {}
+dups = {}
+
+for name in names_1:
+    all_names[name] = True
+
+for name in names_2:
+    try:
+        if all_names[name]:
+            dups[name] = True
+    except:
+        pass
+
+duplicates = dups.keys()
+
+# PROVIDED CODE
+'''
 duplicates = []
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+'''
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
